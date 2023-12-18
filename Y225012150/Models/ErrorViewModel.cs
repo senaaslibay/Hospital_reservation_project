95f6f0906859b60c19e6a9cb6d5e1b0df347b71e
aslibays <senaaslibay@gmail.com>
2023-12-18 17:26:37 +0300

    First commit

---
+++ Y225012150/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Y225012150.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
