e9aa3b0b2bdc448cddb76e87e39f62f4eefed30b
aslibays <senaaslibay@gmail.com>
2023-12-21 21:29:45 +0300

    UserDetails AnaBilimdalları ve Alanlar oluşturuldu, db'ye bağlanmaya çalışılıyor ama daha bağlanamadık

--- Y225012150/Data/Migrations/20231221134624_firstconnection.Designer.cs
+++ Y225012150/Data/Migrations/20231221134624_firstconnection.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Y225012150.Data;
 
@@ -11,9 +12,10 @@ using Y225012150.Data;
 namespace Y225012150.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231221134624_firstconnection")]
+    partial class firstconnection
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Y225012150/Models/Alanlar.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Y225012150.Models
+{
+    public class Alanlar
+    {
+        [Key]
+        public int AlanId { get; set; }
+        [Required]
+        [Display(Name = "Alanı ")]
+        public string AlanAdi { get; set; }
+    }
+}
