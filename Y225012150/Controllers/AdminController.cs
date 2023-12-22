78498f6333af70d32cb71b3b24c4d008ad61dc58
aslibays <senaaslibay@gmail.com>
2023-12-22 20:08:52 +0300

    UserDetail eklendi, Identity yapısı oluşturuldu

---
+++ Y225012150/Controllers/AdminController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Y225012150.Controllers
+{
+    public class AdminController : Controller
+    {
+        [Authorize(Roles = "Admin")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
